9d6bf884d3357a41d24ad73da8e1f1e78b236ae4
iaggocapitanio1 <iaggo.capitanio2gmail.com>
2022-11-13 23:36:26 +0000

    building part payload

--- payloads/part.py
+++ payloads/part.py
@@ -11,6 +11,33 @@ class PartPayload(BasePayload):
         self.sort = kwargs.get('sort', '')
         self.material = kwargs.get('material', '')
         self.amount = kwargs.get('amount', -1)
+        self.length = kwargs.get('length', -1)
+        self.width = kwargs.get('width', -1)
+        self.thickness = kwargs.get('thickness', -1)
+
+    @property
+    def thickness(self) -> float:
+        return self._thickness
+
+    @thickness.setter
+    def thickness(self, thickness: float) -> None:
+        self._thickness = thickness
+
+    @property
+    def width(self) -> float:
+        return self._width
+
+    @width.setter
+    def width(self, width: float) -> None:
+        self._width = width
+
+    @property
+    def length(self) -> float:
+        return self._length
+
+    @length.setter
+    def length(self, length: float) -> None:
+        self._length = length
 
     @property
     def amount(self) -> int:
