5c93d94c8fb7a44b48187f4d0552f2c6aaf2052b
iaggocapitanio1 <iaggo.capitanio2gmail.com>
2022-11-14 00:48:14 +0000

    building part payload

--- payloads/part.py
+++ payloads/part.py
@@ -14,6 +14,61 @@ class PartPayload(BasePayload):
         self.length = kwargs.get('length', -1)
         self.width = kwargs.get('width', -1)
         self.thickness = kwargs.get('thickness', -1)
+        self.tag = kwargs.get("tag", -1)
+        self.nestingFlag = kwargs.get("nestingFlag", False)
+        self.cncFlag = kwargs.get("cncFlag", False)
+        self.f2 = kwargs.get("f2", -1)
+        self.f3 = kwargs.get("f3", -1)
+        self.f4 = kwargs.get("f4", -1)
+        self.f5 = kwargs.get("f5", -1)
+
+    @property
+    def f5(self) -> int:
+        return self._f5
+
+    @f5.setter
+    def f2(self, f5) -> None:
+        self._f5 = f5
+
+    @property
+    def f4(self) -> int:
+        return self._f4
+
+    @f4.setter
+    def f4(self, f4) -> None:
+        self._f4 = f4
+
+    @property
+    def f3(self) -> int:
+        return self._f3
+
+    @f3.setter
+    def f3(self, f3) -> None:
+        self._f3 = f3
+
+    @property
+    def f2(self) -> int:
+        return self._f2
+
+    @f2.setter
+    def f2(self, f2) -> None:
+        self._f2 = f2
+
+    @property
+    def cncFlag(self) -> bool:
+        return self._cncFlag
+
+    @cncFlag.setter
+    def cncFlag(self, cncFlag: bool) -> None:
+        self._cncFlag = cncFlag
+
+    @property
+    def nestingFlag(self) -> bool:
+        return self._nestingFlag
+
+    @nestingFlag.setter
+    def nestingFlag(self, nestingFlag: bool) -> None:
+        self._nestingFlag = nestingFlag
 
     @property
     def thickness(self) -> float:
